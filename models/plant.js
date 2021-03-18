9fcbfe8479ea41716eb3b42850bde34b87fdc3c7
Debbie Brannon <debbie.s.brannon@gmail.com>
2021-03-18 15:44:41 -0500

    Connected my database, can see json in the browser

---
+++ models/plant.js
@@ -0,0 +1,20 @@
+const plant = [
+    {
+        name: 'Swiss Cheese Plant',
+        scientific: 'Monstera deliciosa',
+        propagation: 'cutting'
+    },
+    {
+        name: 'African Violet',
+        scientific: 'Streptocarpus saintpaulia',
+        propagation: 'cutting'
+    },
+    {
+        name: 'Spider Plant',
+        scientific: 'Chlorophytum comosum',
+        propagation: 'cutting'
+    }
+
+]
+
+module.exports = plant
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
+const plant = require('./models/plant.js')
 const PORT = 3000
+
+app.get('/', (req,res) => {
+    res.send('Welcome to the Plant App')
+})
+
+//Index route
+app.get("/plant", (req,res) => {
+    res.send(plant)
+})
 
 app.listen(3000, () => {
     console.log('Server is listening!!!')
